73defba104c4c5d47966c40b3e2271f60742e0ce
Fernando <00017917@uca.edu.sv>
2019-03-24 16:21:36 -0600

    aun no se como validar el email, intento con un public boolean que regrese true o false pero no funciona aun

--- app/src/main/java/com/example/tarea_labo3/Main2Activity.java
+++ app/src/main/java/com/example/tarea_labo3/Main2Activity.java
@@ -3,6 +3,10 @@ package com.example.tarea_labo3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.tarea_labo3.utils.appConstants;
@@ -10,29 +14,51 @@ import com.example.tarea_labo3.utils.appConstants;
 public class Main2Activity extends AppCompatActivity {
 
     private TextView name2, psswd2, mail2, gender2;
+    private EditText invisible;
+    private Button shareB;
+    private String txt1, txt2, txt3, txt4, cadena, cadena2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        invisible = findViewById(R.id.todo);
         name2 = findViewById(R.id.edit_name2);
         psswd2 = findViewById(R.id.edit_psswd2);
         mail2 = findViewById(R.id.edit_mail2);
         gender2 = findViewById(R.id.edit_gender2);
-
         Intent mintent = getIntent();
 
         if(mintent != null){
             String name = mintent.getStringExtra("NOMBRE");
             name2.setText(name);
+            txt1 = name2.getText().toString();
             String key = mintent.getStringExtra("CONTRA");
             psswd2.setText(key);
+            txt2 = psswd2.getText().toString();
             String mail = mintent.getStringExtra("CORREO");
             mail2.setText(mail);
+            txt3 = mail2.getText().toString();
             String gender = mintent.getStringExtra("GENERO");
             gender2.setText(gender);
+            txt4 = gender2.getText().toString();
+
+            cadena = txt1 + "\n" + txt2 + "\n" + txt3 + "\n"+ txt4;
+
+            invisible.setText(cadena);
+            cadena2 = invisible.getText().toString();
+            invisible.setVisibility(View.GONE);
         }
+        shareB = findViewById(R.id.share_btn);
+        shareB.setOnClickListener( v->{
+            Intent i = new Intent();
+            i.setAction(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_TEXT, cadena2);
+            startActivity(i);
+            Log.d("FTP", invisible.getText().toString());
+        });
 
 
     }
